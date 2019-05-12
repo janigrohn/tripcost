122db3c87d0553b506ca32c517efcb71c2951457
Jani Gröhn <janigrohn@Janis-MacBook-Air.local>
2019-05-12 17:32:18 +0300

    TripCost overlay, route modal, heading fixed

--- src/components/tripcost/TripCost.js
+++ src/components/tripcost/TripCost.js
@@ -3,6 +3,17 @@ import VehicleInfo from './VehicleInfo';
 import RouteSelector from './RouteSelector';
 import styled from "styled-components";
 
+const Overlay = styled.div`
+	position: fixed;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+	background-color: ${props => props.isActive ? 'rgba(255, 255, 255, 0.4)' : 'transparent'};
+	pointer-events: ${props => props.isActive ? 'auto' : 'none'};
+	transition: background-color 0.25s ease-in-out;
+`;
+
 const Placeholder = styled.div`
 	display: ${props => props.inUse ? 'block' : 'none'};
 	height: 120px;
@@ -19,6 +30,7 @@ const TripCost = () => {
 
 	return (
 		<>
+			<Overlay isActive={expanded !== null} onClick={expanded !== null ? updateExpanded.bind(null, null) : null} />
 			<Placeholder inUse={expanded === 'vehicleInfo'} />
 			<VehicleInfo
 				toggleExpanded={updateExpanded.bind(null, 'vehicleInfo')}
